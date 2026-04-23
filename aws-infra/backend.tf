e3a9f178b10f17284117573831eaa092696fb4ad
abdul-mannan <mannanmohd2939@gmail.com>
2026-04-23 13:39:44 -0500

    adding files

--- aws-infra/backend.tf
+++ aws-infra/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "cmdstk-terraform-state-batch-1"
-    key    = "shezor/tfstate/terraform.tfstate"
+    key    = "mannan/tfstate/terraform.tfstate"
     region = "us-east-1"
     encrypt = true
   }
