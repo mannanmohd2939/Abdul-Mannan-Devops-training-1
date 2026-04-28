616e21477b4f23a2d3bb5dd48552de3468d2b03f
abdul-mannan <mannanmohd2939@gmail.com>
2026-04-28 00:23:51 -0500

    creating new ec2

--- aws-infra/modules/ec2/outputs.tf
+++ aws-infra/modules/ec2/outputs.tf
@@ -7,13 +7,3 @@ output "ec2_public_ip" {
   description = "Public IP address of the EC2 instance (Elastic IP)"
   value       = aws_eip.this.public_ip
 }
-
-output "secret_name" {
-  description = "Name of the Secrets Manager secret containing the SSH private key"
-  value       = aws_secretsmanager_secret.ssh_key.name
-}
-
-output "ssh_command" {
-  description = "Command to SSH into the instance"
-  value       = "aws secretsmanager get-secret-value --secret-id ${aws_secretsmanager_secret.ssh_key.name} --query SecretString --output text > key.pem && chmod 400 key.pem && ssh -i key.pem ec2-user@${aws_eip.this.public_ip}"
-}
